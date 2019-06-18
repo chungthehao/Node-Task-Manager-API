f0cb705067b141f8b4d5bde1eeed6e0b9db153cd
thehao <chungthehao@gmail.com>
2019-06-19 00:04:53 +0700

    Writing Tests and Assertions

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,8 +1,27 @@
+const { calculateTip } = require('../src/math')
+
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, .3)
+
+    // * C1: Check thủ công
+    // if (total !== 13) {
+    //     throw new Error('Total tip should be 13. Got ' + total)
+    // }
+    // * C2: Check bằng thư viện
+    expect(total).toBe(13)
+})
+
+test('Should calculate total with default tip', () => {
+    const total = calculateTip(10)
+    expect(total).toBe(12.5)
+})
+
+
 // * Setup test case thứ 1, trong func mà throw error thì case đó fail
-test('Hello world!', () => {
+// test('Hello world!', () => {
 
-})
+// })
 
-test('This should fail', () => {
-    throw new Error('Failure!')
-})
+// test('This should fail', () => {
+//     throw new Error('Failure!')
+// })
