099fd6389241d84aefc14f63a753e424d53b38b7
thehao <chungthehao@gmail.com>
2019-06-21 23:56:35 +0700

    Advanced Assertions

--- tests/user.test.js
+++ tests/user.test.js
@@ -29,18 +29,40 @@ beforeEach(async () => {
 // })
 
 test('Should signup a new user', async () => {
-    await request(app).post('/users').send({
+    const response = await request(app).post('/users').send({
         name: 'Henry',
         email: 'henry@example.com',
         password: 'abc123456'
     }).expect(201)
+
+    // 1. Assert that the database was changed correctly (Đảm bảo user thực sự đc save vô db)
+    // - Tức là kiểm tra db tồn tạo 1 document có _id là cái _id được trả về trong response body
+    const userInDB = await User.findById(response.body.user._id) // Nếu ko exist thì return null
+    expect(userInDB).not.toBeNull()
+
+    // 2. Assertions about the response 
+    // - Making sure that the response body matches up with what you were expecting
+    expect(response.body).toMatchObject({ // Phải match những properties đc list ra ở đây
+        user: {
+            name: 'Henry',
+            email: 'henry@example.com',
+        },
+        token: userInDB.tokens[0].token
+    })
+    // expect(response.body.user.name).toBe('Henry') // Chỉ dành cho assert 1 property
+
+    expect(userInDB).not.toBe('abc123456') // Đảm bảo pw ko lưu trần chuồng
 })
 
 test('Should login existing user', async () => {
-    await request(app).post('/users/login').send({
+    const response = await request(app).post('/users/login').send({
         email: userOne.email,
         password: userOne.password
     }).expect(200)
+
+    // Validate new token (2nd, index là 1) is saved
+    const userInDB = await User.findById(userOneId)
+    expect(response.body.token).toBe(userInDB.tokens[1].token)
 })
 
 test('Should not login nonexistent user', async () => {
@@ -72,6 +94,10 @@ test('Should delete account for user', async () => {
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
+
+    // Validate user is removed
+    const userInDB = await User.findById(userOneId)
+    expect(userInDB).toBeNull()
 })
 
 test('Should not delete account for unauthenticated user', async () => {
