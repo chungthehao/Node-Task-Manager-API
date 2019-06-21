7904a0c6c110912eb10f40bf8422fc8db10903de
thehao <chungthehao@gmail.com>
2019-06-21 21:17:32 +0700

    Jest Setup and Teardown

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,10 +1,44 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+// - Prepare user data
+const userOne = {
+    name: 'Hao Chung',
+    email: 'hao@example.com',
+    password: 'qwerty123456'
+}
+
+beforeEach(async () => {
+    // - Xóa hết các user hiện có trong db test
+    await User.deleteMany()
+
+    // - A very specific data in db that we can use when testing (login,...)
+    await (new User(userOne)).save() // chỉ cần new User(userOne).save() thôi là chạy ok r!
+})
+
+// afterEach(() => {
+//     console.log('afterEach')
+// })
 
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
         name: 'Henry',
-        email: 'henry2@example.com',
+        email: 'henry@example.com',
         password: 'abc123456'
     }).expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should not login nonexistent user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: 'This is not my password'
+    }).expect(400)
 })
