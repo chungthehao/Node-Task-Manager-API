bf4ed8373798214b5e9c404cf6b848698c8ad7a6
thehao <chungthehao@gmail.com>
2019-06-22 00:52:10 +0700

    Mocking Libraries

---
+++ tests/__mocks__/@sendgrid/mail.js
@@ -0,0 +1,14 @@
+/**
+ * - Giả npm module để khi chạy test, nó chui vô đây tìm mấy cái này chạy chứ ko chạy cái thật
+ * - Tên thư mục và func rất quan trọng
+ * - Chi tiết đọc thêm docs: https://jestjs.io/docs/en/manual-mocks
+ */
+
+module.exports = {
+    setApiKey() {
+
+    },
+    send() {
+
+    }
+}
