968a217a01498527dc4bcf57af8dac3f239b66ee
thehao <chungthehao@gmail.com>
2019-06-22 12:12:35 +0700

    Testing with Task Data

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose') //Để mình tạo 'ObjectId'
 const jwt = require('jsonwebtoken')
 const User = require('../../src/models/user')
+const Task = require('../../src/models/task')
 
 // - Prepare user data
 const userOneId = new mongoose.Types.ObjectId()
@@ -14,16 +15,59 @@ const userOne = {
     }]
 }
 
+const userTwoId = new mongoose.Types.ObjectId()
+const userTwo = {
+    _id: userTwoId,
+    name: 'Len Chay Hu',
+    email: 'lenchayhu@example.com',
+    password: 'myhouse111!1',
+    tokens: [{
+        token: jwt.sign({ _id: userTwoId/*.toString()*/ }, process.env.JWT_SECRET, { expiresIn: '7 days' })
+    }]
+}
+
+const taskOne = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'First task',
+    completed: false,
+    owner: userOne._id
+}
+
+const taskTwo = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Second task',
+    completed: true,
+    owner: userOne._id
+}
+
+const taskThree = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Third task',
+    completed: true,
+    owner: userTwo._id
+}
+
 const setupDatabase = async () => {
-    // - Xóa hết các user hiện có trong db test
+    // - Xóa hết các user, task hiện có trong db test
     await User.deleteMany()
+    await Task.deleteMany()
 
     // - A very specific data in db that we can use when testing (login,...)
     await (new User(userOne)).save() // chỉ cần new User(userOne).save() thôi là chạy ok r!
+    await new User(userTwo).save()
+    // Tạo sẵn các specific task để test (taskOne, taskTwo là của userOne; taskThree là của userTwo)
+    await new Task(taskOne).save()
+    await new Task(taskTwo).save()
+    await new Task(taskThree).save()
 }
 
 module.exports = {
     userOneId,
     userOne,
-    setupDatabase
+    userTwoId,
+    userTwo,
+    setupDatabase,
+    taskOne,
+    taskTwo,
+    taskThree
 }
--- tests/task.test.js
+++ tests/task.test.js
@@ -1,7 +1,12 @@
 const app = require('../src/app')
 const request = require('supertest')
 const Task = require('../src/models/task')
-const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
+const { 
+    userOne, 
+    userTwo, 
+    setupDatabase, 
+    taskOne, 
+} = require('./fixtures/db')
 
 beforeEach(setupDatabase)
 
@@ -18,4 +23,26 @@ test('Should create task for user', async () => {
 
     // - Check xem cái task mới đc create, có 'completed' đúng là = false theo default ko?
     expect(task.completed).toBe(false)
+})
+
+test('Should fetch user tasks', async () => {
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+
+    expect(response.body.length).toBe(2)
+})
+
+test('Should not delete other users tasks', async () => {
+    await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404)
+
+    // Making sure là taskOne vẫn còn trong db
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
 })
